714615dc9179d0ac1deb8e1fd7c46c8092eade0d
Mateus <mateus_freitascorreia@hotmail.com>
2024-04-27 23:32:44 -0300

    feat: documentação swagger

--- src/main/java/com/example/desafio_rpe/DesafioRpeApplication.java
+++ src/main/java/com/example/desafio_rpe/DesafioRpeApplication.java
@@ -1,9 +1,12 @@
 package com.example.desafio_rpe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Desafio  RPE", version = "1", description = "Projeto desenvolvido para a vaga de Estágio Back-End"))
 public class DesafioRpeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/desafio_rpe/controller/StoreController.java
+++ src/main/java/com/example/desafio_rpe/controller/StoreController.java
@@ -3,6 +3,10 @@ package com.example.desafio_rpe.controller;
 import com.example.desafio_rpe.dto.StoreDto;
 import com.example.desafio_rpe.model.Store;
 import com.example.desafio_rpe.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,7 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", produces = {"application/json"})
+@Tag(name = "store-api")
 public class StoreController {
     private final StoreService storeService;
 
@@ -22,6 +27,10 @@ public class StoreController {
 
     private final Logger log = LoggerFactory.getLogger(StoreController.class);
 
+    @Operation(summary = "Realiza o cadastro de lojas", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Loja criada com sucesso")
+    })
     @PostMapping("/store")
     public ResponseEntity<Store> createStore(@RequestBody StoreDto newStore) {
         log.debug("Create Store executed");
@@ -30,6 +39,10 @@ public class StoreController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Retorna a lista de todas as lojas cadastradas", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista obtida com sucesso")
+    })
     @GetMapping("/store")
     public ResponseEntity<List<Store>> getAll(){
         List<Store> response = storeService.getAll();
